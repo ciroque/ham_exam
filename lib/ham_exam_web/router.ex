8ac8ee3078d62a0533255dd368f12644fc305c36
Steve Wagner <ciroque@live.com>
2019-11-30 21:09:28 -0800

    Hooks up React

--- lib/ham_exam_web/router.ex
+++ lib/ham_exam_web/router.ex
@@ -16,7 +16,7 @@ defmodule HamExamWeb.Router do
   scope "/", HamExamWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
